75f5c981c89ba823fbf711b5c6a4eeaa55a88e67
Naba Das <naba@Nabas-MacBook-Air.local>
2022-10-25 15:08:14 +0530

    update user

--- testy/docker-entrypoint.sh
+++ testy/docker-entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf -
 	chown -R www-data /var/www/html
-	if [ -n "${MYSQL_HOSTNAME}" ]; then
-		sed -i -e "s/localhost/$MYSQL_HOSTNAME/g" /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e "s/your database username/$MYSQL_USER/g" /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e "s/your database password/$MYSQL_PASSWORD/g" /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e "s/your database name/$MYSQL_DATABASE/g" /var/www/html/system/tastyigniter/config/database.php
+	HOST=`hostname`
+    NAME=`echo $HOST | cut -c9-`
+
+	if [ -n "${NAME}" ]; then
+		sed -i -e "s/localhost/$NAME/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database username/$MARIADB_USER/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database password/$MARIADB_PASSWORD/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database name/$MARIADB_DATABASE/g" /var/www/html/system/tastyigniter/config/database.php
 	fi
 fi
 
