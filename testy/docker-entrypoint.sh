bdf1ac69663a7546cd4776ad5c2f3621e1616f7e
Naba Das <naba@Nabas-MacBook-Air.local>
2022-10-25 16:50:55 +0530

    update htaccess

--- testy/docker-entrypoint.sh
+++ testy/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf -
 	chown -R www-data /var/www/html
 	HOST=`hostname`
-    NAME=`echo $HOST | cut -c9-`
+    NAME=`echo $HOST | cut -c5-`
 
 	if [ -n "${NAME}" ]; then
 		sed -i -e "s/localhost/$NAME/g" /var/www/html/system/tastyigniter/config/database.php
