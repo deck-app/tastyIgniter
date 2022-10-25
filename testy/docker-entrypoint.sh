41e28ae13e6d491b2a87c10d10cde0f696f26444
Naba Das <naba@Nabas-MacBook-Air.local>
2022-10-25 19:09:13 +0530

    database config

--- testy/docker-entrypoint.sh
+++ testy/docker-entrypoint.sh
@@ -2,14 +2,13 @@
 set -e
 
 if [ ! -e '/var/www/html/index.php' ]; then
+	echo >&2 "Tastyigniter not found in $(pwd) ..."
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf - 2> /dev/null
 	chown -R www-data:www-data /var/www/html  2> /dev/null
 	HOST=`hostname`
-    NAME=`echo $HOST | cut -c6-`
-	echo $HOST
-	echo $NAME
+    NAME=`echo $HOST | cut -c7-`
 	sed -i "s/{DB_HOSTNAME}/$NAME/g" /var/www/html/system/tastyigniter/config/database.php
-	
+	echo "Application installed successfully"
 fi
 
 exec "$@"
