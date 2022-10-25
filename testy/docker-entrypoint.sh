8f7d7fa61086d0a4389601e6f3598d71c0b70f5c
Naba Das <naba@Nabas-MacBook-Air.local>
2022-10-25 18:07:52 +0530

    database config

--- testy/docker-entrypoint.sh
+++ testy/docker-entrypoint.sh
@@ -6,7 +6,10 @@ if [ ! -e '/var/www/html/index.php' ]; then
 	chown -R www-data:www-data /var/www/html  2> /dev/null
 	HOST=`hostname`
     NAME=`echo $HOST | cut -c6-`
+	echo $HOST
+	echo $NAME
 	sed -i "s/{DB_HOSTNAME}/$NAME/g" /var/www/html/system/tastyigniter/config/database.php
+	
 fi
 
 exec "$@"
